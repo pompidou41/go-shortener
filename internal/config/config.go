ee2005b7a0cf751a8794b8916950142bd90d76f7
Shamsudin Musaev <selecty@MacBookPro.lan>
2026-05-06 00:14:33 +0300

    mvp

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,11 +4,13 @@ import "os"
 
 type Config struct {
 	SecretSalt string
+	Port       string
 }
 
 func New() *Config {
 	return &Config{
-		SecretSalt: string(getEnv("SECRET_SALT", "")),
+		SecretSalt: string(getEnv("SECRET_SALT", "salt")),
+		Port:       string(getEnv("PORT", "3000")),
 	}
 }
 
--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"pompidou17/shortener/internal/config"
+	"pompidou17/shortener/internal/hash"
 
 	"github.com/joho/godotenv"
 )
+
+type url struct {
+	id       int64
+	shortUrl string
+	longUrl  string
+}
+
+var db []url
 
 func init() {
 	if err := godotenv.Load(); err != nil {
@@ -14,10 +26,57 @@ func init() {
 }
 
 func shortenHandler(w http.ResponseWriter, r *http.Request) {
-	return
+	type RequestBody struct {
+		URL *string `json:"url"`
+	}
+	// TODO: refactor secrets
+	salt := config.New().SecretSalt
+
+	jsonBlob, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Body read err:", err)
+		http.Error(w, "Body read err:"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var reqBody RequestBody
+	err = json.Unmarshal(jsonBlob, &reqBody)
+
+	if err != nil {
+		log.Println("Body parse err:", err)
+		http.Error(w, "Body parse err:"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	longUrl := reqBody.URL
+	if longUrl == nil || *reqBody.URL == "" {
+		log.Println("URL not provided in body or it is empty")
+		http.Error(w, "URL not provided in body or it is empty", http.StatusBadRequest)
+		return
+	} else {
+		id := int64(len(db) + 1)
+		shortUrl := hash.EncodeId(id, salt)
+
+		url := url{
+			id:       id,
+			longUrl:  *longUrl,
+			shortUrl: shortUrl,
+		}
+
+		db = append(db, url)
+		log.Println(longUrl, url, db)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(shortUrl))
+		return
+	}
 }
 
 func main() {
+	conf := config.New()
+
+	port := ":" + conf.Port
+
 	http.HandleFunc("/shorten", shortenHandler)
-	log.Fatal((http.ListenAndServe(":3006", nil)))
+	log.Fatal((http.ListenAndServe(port, nil)))
 }
